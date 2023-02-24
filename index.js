854465f8d01ce426affe9b41f76adaf61e1130bf
Pritam Chakraborty <59708888+pritamcy@users.noreply.github.com>
2023-02-24 22:47:58 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-import initScrollReveal from "https://github.com/pritamcy/pritamcy.github.io/tree/main/scripts/scrollReveal";
-import initTiltEffect from "https://github.com/pritamcy/pritamcy.github.io/tree/main/scripts/tiltAnimation";
-import { targetElements, defaultProps } from "https://github.com/pritamcy/pritamcy.github.io/tree/main/data/scrollRevealConfig";
+import initScrollReveal from "https://github.com/pritamcy/pritamcy.github.io/tree/main/scripts/scrollReveal.js";
+import initTiltEffect from "https://github.com/pritamcy/pritamcy.github.io/tree/main/scripts/tiltAnimation.js";
+import { targetElements, defaultProps } from "https://github.com/pritamcy/pritamcy.github.io/tree/main/data/scrollRevealConfig.js";
 
 initScrollReveal(targetElements, defaultProps);
 initTiltEffect();
